1a166d41e9e250bb2b4f440486e5dbd7ac28382b
Gunner Cook-Dumas <gtcdumas@cox.net>
2025-08-22 11:27:28 -0400

    merge prepare

---
+++ src/agents/model.py
@@ -0,0 +1,19 @@
+import tkinter as tk
+import numpy as np
+import pandas as pd
+import mesa
+
+import 
+
+class UUVModel(mesa.Model):
+    """UUV model testing class"""
+    
+    def __init__(self, n, canvas,*args, seed = None, rng = None, **kwargs):
+        super().__init__(*args, seed=seed, rng=rng, **kwargs)
+        self.num_agents = n
+        self.canvas = canvas
+        UUVAgent.create_agents(model=self, n=n, canvas=self.canvas)
+
+    def step(self):
+        """advance model by one step"""
+        self.agents.do("move_to_target")
